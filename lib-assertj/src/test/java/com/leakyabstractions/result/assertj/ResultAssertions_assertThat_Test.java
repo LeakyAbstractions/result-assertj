1e58f4fc0a0938aecdebe0d2f4e021da5da5fee7
Guillermo Calvo <guillermo@guillermo.dev>
2022-12-19 13:40:38 +0100

    Refactor to improve code coverage

---
+++ lib-assertj/src/test/java/com/leakyabstractions/result/assertj/ResultAssertions_assertThat_Test.java
@@ -0,0 +1,26 @@
+
+package com.leakyabstractions.result.assertj;
+
+import static com.leakyabstractions.result.Results.success;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link ResultAssertions}.
+ *
+ * @author Guillermo Calvo
+ */
+@DisplayName("ResultAssertions assertThat")
+class ResultAssertions_assertThat_Test {
+
+    @Test
+    @SuppressWarnings("static")
+    void should_return_result_assertions() {
+        // When
+        final ResultAssertions assertions = new ResultAssertions();
+        // Then
+        assertThat(assertions.assertThat(success("SUCCESS"))).isInstanceOf(ResultAssert.class);
+    }
+}

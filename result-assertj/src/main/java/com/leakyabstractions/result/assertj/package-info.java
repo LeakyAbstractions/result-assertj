6734f84a0f42804c72b611cc0008362f8d3ee4c5
Guillermo Calvo <guillermo@guillermo.dev>
2024-09-02 18:28:18 +0200

    Update javadoc

--- result-assertj/src/main/java/com/leakyabstractions/result/assertj/package-info.java
+++ result-assertj/src/main/java/com/leakyabstractions/result/assertj/package-info.java
@@ -1,18 +1,20 @@
 /**
- * Provides AssertJ assertions for {@link com.leakyabstractions.result.api.Result} objects.
+ * Provides fluent assertions for {@link com.leakyabstractions.result.api.Result} objects.
  * <p>
  * <img src="https://dev.leakyabstractions.com/result-api/result.svg" alt="Result Library">
  * <h2>Fluent Assertions for Result</h2>
  * <p>
- * You can use fluent assertions for Result objects in your unit tests to enhance the readability and expressiveness of
- * your tests. These assertions are based on <a href="https://assertj.github.io/">AssertJ</a>, an open-source Java
- * library that offers a fluent API for writing assertions in test cases.
+ * You can use fluent assertions for Result objects to enhance the readability and expressiveness of your unit tests.
+ * These assertions are based on <a href="https://assertj.github.io/">AssertJ</a>, an open-source Java library that
+ * offers a fluent API for writing assertions in test cases.
  *
  * @author <a href="https://guillermo.dev/">Guillermo Calvo</a>
  * @see <a href="https://result.leakyabstractions.com/add-ons/assertj">Quick guide</a>
+ * @see <a href="https://leanpub.com/result/">Free book</a>
  * @see <a href="https://github.com/LeakyAbstractions/result-assertj/">Source code</a>
  * @see com.leakyabstractions.result.assertj.ResultAssertions#assertThat(com.leakyabstractions.result.api.Result)
- * @see com.leakyabstractions.result.api.Result
+ *     assertThat
+ * @see com.leakyabstractions.result.api.Result Result
  */
 
 package com.leakyabstractions.result.assertj;
